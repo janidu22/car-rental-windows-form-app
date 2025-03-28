99c940233d127e2b2ee01ccbd4fd90bbbf848f91
janidu22 <janidudhakshi222@gmail.com>
2025-03-28 16:56:14 +0530

    input validation is done

--- CarRental app/Form1.Designer.cs
+++ CarRental app/Form1.Designer.cs
@@ -214,7 +214,6 @@
             this.Controls.Add(this.Title);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- CarRental app/Form1.cs
+++ CarRental app/Form1.cs
@@ -10,12 +10,68 @@ using System.Windows.Forms;
 
 namespace CarRental_app
 {
-    public partial class Form1: Form
+    public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void BtnBooking_Click(object sender, EventArgs e)
+        {
+            // customer details/billing details
+            string CustomerName = TxtCustomerName.Text;
+            string CustomerNic = TxtCustomerNic.Text;
+            DateTime dayRented = DtRented.Value;
+            DateTime dayReturned = DtReturned.Value;
+            double cost = 0;
+
+            // input validation
+            var isValid = true;
+
+            if (string.IsNullOrEmpty(CustomerName) 
+                || string.IsNullOrEmpty(CustomerNic)
+                || !double.TryParse(TxtCost.Text, out cost)                 
+                )
+            {
+                isValid = false;
+                MessageBox.Show("All the inputs are required!..");
+            }
+
+
+
+            if (dayRented > dayReturned)
+            {
+                isValid = false;
+                MessageBox.Show("opss, invalid date selection");
+
+            if(cost < 0)
+            {
+                isValid =false;
+                MessageBox.Show("cost has to be higher than 0 LKR");
+            }
+
+            }
+
+            if (CbTypeOfCar.SelectedItem == null)
+            {
+                isValid = false;
+                MessageBox.Show("please select are car");
+            }
+
+            var carType = CbTypeOfCar.SelectedItem.ToString();
+            
+            if (isValid)
+            {
+                MessageBox.Show($"*****booking confirmed*****\n\r" +
+                    $"customer Name: {CustomerName}\n\r" +
+                    $"selected Car: {carType}\n\r" +
+                    $"day Rented: {dayRented}\n\r" +
+                    $"Day Return: {dayReturned}\n\r" +
+                    $"Total Cost: {cost} LKR\n\r" +
+                    $"Thanks for choosing us, have a nice day {CustomerName}");
+            }
+
+        }
     }
 }

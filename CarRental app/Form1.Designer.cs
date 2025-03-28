85a87628d974e7d8a3992a214a50c01fff00be91
janidu22 <janidudhakshi222@gmail.com>
2025-03-28 10:22:42 +0530

    added new component to the form 1 GUI

--- CarRental app/Form1.Designer.cs
+++ CarRental app/Form1.Designer.cs
@@ -38,6 +38,8 @@
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // Title
@@ -67,7 +69,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(52, 151);
+            this.label1.Location = new System.Drawing.Point(55, 151);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(217, 32);
             this.label1.TabIndex = 2;
@@ -125,7 +127,7 @@
             "Nissan Altima",
             "Hyundai Elantra",
             "Mazda 3"});
-            this.comboBox1.Location = new System.Drawing.Point(58, 301);
+            this.comboBox1.Location = new System.Drawing.Point(58, 465);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(388, 37);
             this.comboBox1.TabIndex = 7;
@@ -135,7 +137,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("MS Reference Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(52, 266);
+            this.label3.Location = new System.Drawing.Point(55, 417);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(220, 32);
             this.label3.TabIndex = 8;
@@ -144,18 +146,40 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(410, 410);
+            this.button1.Location = new System.Drawing.Point(682, 404);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(239, 59);
             this.button1.TabIndex = 9;
             this.button1.Text = "Confirm Booking";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS Reference Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(55, 250);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(190, 32);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Customer NIC";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(58, 305);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(388, 45);
+            this.textBox2.TabIndex = 11;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1086, 565);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.comboBox1);
@@ -185,6 +209,8 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- CarRental app/Form1.cs
+++ CarRental app/Form1.cs
@@ -41,5 +41,10 @@ namespace CarRental_app
         {
 
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
